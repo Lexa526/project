f15a5f0e998b95086bc2c6443946c4962a163b5d
Lexa526 <aleksejbernatovic@gmail.com>
2022-03-13 16:01:00 +0300

    txt

--- js/main.js
+++ js/main.js
@@ -1,12 +1,54 @@
 let red = 0
 let green = 0
 let blue = 0
-setInterval(()=>{
-    document.querySelector("html").style.background=`rgb(${red},${green},${blue})`
-    function getRandomArbitrary(min, max) {
-        return Math.random() * (max - min) + min;
-      }
-    red = getRandomArbitrary(1,255)
-    blue = getRandomArbitrary(1,255)
-    green = getRandomArbitrary(1,255)
-})
+
+setInterval(() => {
+  if (red <= 255) {
+    for (let i = 0; i <= 255; i++) {
+      setTimeout(() => {
+        red++
+        document.querySelector("html").style.background = `rgb(${red},${green},${blue})`
+      }, 50*i);
+    }
+  } else {
+    for (let i = 0; i <= 255; i++) {
+      
+      setTimeout(() => {
+        red--
+        document.querySelector("html").style.background = `rgb(${red},${green},${blue})`
+      }, 50*i);
+    }
+  }
+  if (blue <= 255) {
+    for (let i = 0; i <= 255; i++) {
+      setTimeout(() => {
+        blue++
+        document.querySelector("html").style.background = `rgb(${blue},${green},${blue})`
+      }, 50*i);
+    }
+  } else {
+    for (let i = 0; i <= 255; i++) {
+      
+      setTimeout(() => {
+        blue--
+        document.querySelector("html").style.background = `rgb(${blue},${green},${blue})`
+      }, 50*i);
+    }
+  }
+  if (green <= 255) {
+    for (let i = 0; i <= 255; i++) {
+      setTimeout(() => {
+        green++
+        document.querySelector("html").style.background = `rgb(${green},${green},${blue})`
+      }, 50*i);
+    }
+  } else {
+    for (let i = 0; i <= 255; i++) {
+      
+      setTimeout(() => {
+        green--
+        document.querySelector("html").style.background = `rgb(${green},${green},${blue})`
+      }, 50*i);
+    }
+  }
+}, 15000)
