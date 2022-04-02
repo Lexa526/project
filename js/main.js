93c7a4b8694cbaa52a1be6c19ff2912fdb3e61c5
Lexa <86785362+Lexa526@users.noreply.github.com>
2022-04-02 14:14:06 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,25 +1,34 @@
-let aButton = document.querySelector(".stepen_result");
-aButton.addEventListener("click", (e) => {
-  let a = document.querySelector(".a_stepen").value;
-  let b = document.querySelector(".b_stepen").value;
-  let result = a ** b;
-  document.querySelector(".powers-answer").textContent = `= ${result} `;
+let bomb = document.querySelector(".bomb");
+bomb.addEventListener("click", (e) => {
+    document.body.innerHTML = " ";
 });
-/* Периметр .. */
-let bButton = document.querySelector(".p_s_result");
-bButton.addEventListener("click", (e) => {
-  let a = document.querySelector(".a_p_s").value;
-  let result = a * 4;
-  document.querySelector(".p_s_a").textContent = `= ${result}`;
+let clear = document.querySelector(".clear");
+clear.addEventListener("click", (e) => {
+    for (let i = 0; i < document.getElementsByTagName('input').length; i++) {
+        document.getElementsByTagName('input')[i].value = " ";
+    }
 });
-let cButton = document.querySelector(".p_r_result");
-cButton.addEventListener("click", (e) => {
-  let a = document.querySelector(".a_p_r").value;
-  let b = document.querySelector(".b_p_r").value;
+window.onscroll = function () { scrollFunction() };
 
-  let result = 2 * (a * 1 + b * 1);
-  console.log(a);
-  console.log(b);
-  console.log(result);
-  document.querySelector(".p_r_a").textContent = `= ${result} `;
-});
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("header").style.backgroundColor = "rgba(0,0,0,0.9)";
+        if (document.documentElement.scrollTop > 90) {
+            document.getElementById("header").style.backgroundColor = "rgba(0,0,0,0.7)";
+        }
+        if (document.documentElement.scrollTop > 130) {
+            document.getElementById("header").style.backgroundColor = "rgba(0,0,0,0.5)";
+        }
+        if (document.documentElement.scrollTop > 170) {
+            document.getElementById("header").style.backgroundColor = "rgba(0,0,0,0.3)";
+        }
+        if (document.documentElement.scrollTop > 210) {
+            document.getElementById("header").style.backgroundColor = "rgba(0,0,0,0.1)";
+        }
+        if (document.documentElement.scrollTop > 250) {
+            document.getElementById("header").style.backgroundColor = "rgba(0,0,0,0)";
+        }
+    } else {
+        document.getElementById("header").style.backgroundColor = "rgba(0,0,0,100)";
+    }
+}
